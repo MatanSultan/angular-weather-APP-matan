943aafa3ed51327d654135ff0296703b792fa75b
MatanSultan <88273616+MatanSultan@users.noreply.github.com>
2022-09-18 21:53:46 +0300

    first commit

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,7 +1,7 @@
-import { WeatherData } from '../models/weather.models';
+import { WeatherData } from './../models/weather.models';
 import { HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from 'src/environments/environment';
 import { Observable} from 'rxjs';
 
 @Injectable({
